fcc3d8a0eac3369e459c0cd445dc67b581aef745
anand-184 <anandita9464@gmail.com>
2024-06-24 20:04:47 +0530

    commit

---
+++ app/src/main/java/com/anand/bottom_navigation_task/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.anand.bottom_navigation_task
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.anand.bottom_navigation_task.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
+    var navController: NavController? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        navController = findNavController(R.id.host)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding?.bottomNav?.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.f1-> navController?.navigate(R.id.fragment_login)
+                R.id.f2->navController?.navigate(R.id.fragment_sign)
+            }
+            return@setOnItemSelectedListener true
+        }
+
+    }
+}
